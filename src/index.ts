6902813db23826594623f9f2d104c145b4fd69f0
Naresh Parshotam <naresh.parshotam@raynsecure.com>
2025-10-10 11:30:31 +0800

    Fix export types for all components

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 // Main exports for the notifications module
-export { default as EmailNotifications } from './components/EmailNotifications';
-export { default as EmailNotificationsWrapper } from './components/EmailNotificationsWrapper';
+export { EmailNotifications } from './components/EmailNotifications';
+export { EmailNotificationsWrapper } from './components/EmailNotificationsWrapper';
 export { default as LessonReminderSettings } from './components/LessonReminderSettings';
 export { default as LessonReminderSettingsPage } from './components/LessonReminderSettingsPage';
 export { default as LessonReminderSettingsWrapper } from './components/LessonReminderSettingsWrapper';
-export { default as NotificationCenter } from './components/NotificationCenter';
-export { default as NotificationItem } from './components/NotificationItem';
-export { default as NotificationSettings } from './components/NotificationSettings';
+export { NotificationCenter } from './components/NotificationCenter';
+export { NotificationItem } from './components/NotificationItem';
+export { NotificationSettings } from './components/NotificationSettings';
 
 // Hooks
 export { useNotificationSettings } from './hooks/useNotificationSettings';
