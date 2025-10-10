dc445c78655cab8ceb6cab61383a9faf9bd0efb7
Naresh Parshotam <naresh.parshotam@raynsecure.com>
2025-10-10 10:54:31 +0800

    Add module exports

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// Main exports for the notifications module
+export { default as EmailNotifications } from './components/EmailNotifications';
+export { default as EmailNotificationsWrapper } from './components/EmailNotificationsWrapper';
+export { default as LessonReminderSettings } from './components/LessonReminderSettings';
+export { default as LessonReminderSettingsPage } from './components/LessonReminderSettingsPage';
+export { default as LessonReminderSettingsWrapper } from './components/LessonReminderSettingsWrapper';
+export { default as NotificationCenter } from './components/NotificationCenter';
+export { default as NotificationItem } from './components/NotificationItem';
+export { default as NotificationSettings } from './components/NotificationSettings';
+
+// Hooks
+export { default as useNotificationSettings } from './hooks/useNotificationSettings';
+export { default as useNotifications } from './hooks/useNotifications';
+
+// Types
+export * from './types';
+
+// Utils
+export * from './utils/supabase';
+export * from './lib/emailService';
